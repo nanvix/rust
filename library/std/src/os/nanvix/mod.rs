a9ce194fe75c6a48e7804c6433b55c370f7aab4c
Pedro Henrique Penna <ppenna@microsoft.com>
2025-05-29 22:18:27 -0700

    Dummy std::os::ffi

--- library/std/src/os/nanvix/mod.rs
+++ library/std/src/os/nanvix/mod.rs
@@ -3,3 +3,15 @@
 
 #[stable(feature = "raw_ext", since = "1.1.0")]
 pub use ::sys::*;
+
+pub mod ffi;
+
+/// A prelude for conveniently writing platform-specific code.
+///
+/// Includes all extension traits, and some important type definitions.
+#[stable(feature = "rust1", since = "1.0.0")]
+pub mod prelude {
+    #[doc(no_inline)]
+    #[stable(feature = "rust1", since = "1.0.0")]
+    pub use super::ffi::{OsStrExt, OsStringExt};
+}
