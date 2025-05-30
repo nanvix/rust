6bfcf3fd1063120aaf6fe855954d261f5a2af781
Pedro Henrique Penna <ppenna@microsoft.com>
2025-05-29 22:10:15 -0700

    Dummy std::sys::io::io_slice

--- library/std/src/sys/io/mod.rs
+++ library/std/src/sys/io/mod.rs
@@ -11,6 +11,9 @@ mod io_slice {
         } else if #[cfg(target_os = "wasi")] {
             mod wasi;
             pub use wasi::*;
+        } else if #[cfg(target_os = "nanvix")] {
+            mod nanvix;
+            pub use nanvix::*;
         } else {
             mod unsupported;
             pub use unsupported::*;
