3e95af322b97fb951963d33b3a6d2db79afb8894
Pedro Henrique Penna <ppenna@microsoft.com>
2025-06-30 16:48:05 -0700

    Simple impl for is_terminal

--- library/std/src/sys/io/is_terminal/nanvix.rs
+++ library/std/src/sys/io/is_terminal/nanvix.rs
@@ -1,5 +1,15 @@
-use crate::os::fd::AsFd;
+use ::syscall::sysapi::unistd::{STDERR_FILENO, STDIN_FILENO, STDOUT_FILENO};
 
-pub fn is_terminal(_fd: &impl AsFd) -> bool {
-    false
+use crate::os::fd::{AsFd, AsRawFd};
+
+pub fn is_terminal(fd: &impl AsFd) -> bool {
+    let fd = fd.as_fd();
+    if fd.as_raw_fd() == STDIN_FILENO
+        || fd.as_raw_fd() == STDOUT_FILENO
+        || fd.as_raw_fd() == STDERR_FILENO
+    {
+        true
+    } else {
+        false
+    }
 }
